dfaa820ad0e378ff97b8ca7709447e9b8142f9d2
Lutin <tclutin01@gmail.com>
2024-03-24 20:31:28 +0500

    Initial commit

---
+++ src/main/java/org/example/accounts/AccountService.java
@@ -0,0 +1,20 @@
+package org.example.accounts;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountService {
+    private static Map<String, UserProfile> loginToProfile = new HashMap<>();
+
+    static {
+        loginToProfile.put("admin", new UserProfile("admin", "1", "email@email.ru"));
+    }
+    public static void AddNewUser(UserProfile user) {
+        loginToProfile.put(user.getLogin(), user);
+    }
+
+    public static UserProfile getUserByLogin(String login) {
+        return loginToProfile.get(login);
+    }
+
+}
